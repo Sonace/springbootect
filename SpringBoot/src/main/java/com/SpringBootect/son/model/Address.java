cefe92114af5c6d17b2613c5d2813366834ea19b
son97.it@gmail.com <ngovanson>
2018-06-01 16:40:23 +0700

    commit

--- SpringBoot/src/main/java/com/SpringBootect/son/model/Address.java
+++ SpringBoot/src/main/java/com/SpringBootect/son/model/Address.java
@@ -5,9 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-
-
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -21,11 +23,13 @@ public class Address {
 	private int user_id;
 
 	@NotNull
-	private int phone_number;
+	@Pattern(regexp = "^[0-9\\s- \\+]{8,13}$")
+	private String phone_number;
 	
 	@Email
 	private String email;
 	
 	@NotNull
-	private int fax;
+	@Pattern(regexp = "^[0-9\\s- \\+]{8,13}$")
+	private String fax;
 }
